afe3db50143c1ade84424de40caafce8a85b8ca1
Sebastian David Estrada Ojeda <sdavide1010@gmail.com>
2021-10-23 01:21:41 -0500

    Automation class project update

--- src/test/java/Booking/Steps/Conexion.java
+++ src/test/java/Booking/Steps/Conexion.java
@@ -1,7 +1,10 @@
 package Booking.Steps;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.Before;
 import net.thucydides.core.annotations.Step;
@@ -13,14 +16,18 @@ public class Conexion {
 	@Before
 	@Step
 	public WebDriver abrirNavegador() {
-		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver95.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.navigate().to("https://www.booking.com/index.es.html");
-		
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
-		
+		driver.navigate().to("https://www.booking.com/index.es.html");		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);		
+//		WebElement searchInput = driver.findElement(By.id("ss"));
+//		searchInput.sendKeys("Ucrania");
+//		searchInput.submit();
+//		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	
+//		WebElement fiveStarsFilter = driver.findElement(By.id("__bui-102"));
+//		System.out.println("Executed action");
+//		fiveStarsFilter.click();		
 		return this.driver;
 
 	}
